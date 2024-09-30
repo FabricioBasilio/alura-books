aecd50392eef3112ad325e964014f4bb132b70ca
Fabricio Basilio <fabriciobasilioo@gmail.com>
2024-09-30 14:31:52 -0300

    Usando os 2 primeiros métodos de fetch
    Usando o método then para o caso de uma Promise resolve e o método catch no caso de uma Promise reject.

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-let requisicao = fetch("https://viacep.com.br/ws/01001000/json/")
-.then(resposta => resposta.json())
+let requisicao = fetch("https://viacep.com.br/ws/01002324/json/")
+.then(resposta => {
+    console.log("Primeiro then (resposta json)");
+    return resposta.json();
+})
+.then(r => {
+    if (r.erro) {
+        throw Error("O CEP é inexistente!");
+    }
+    else {
+        console.log("No segundo then (CEP não é inexistente):");
+        console.log(r);
+    }
+    
+})
+.catch(error => {
+    console.log("No catch, quando foi usado o throw error");
+    console.log(error)
+});
 
 console.log(requisicao)
